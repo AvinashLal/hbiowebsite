eb73e8addd96cb5bb9e09b6edd92a2dcb3a84d2c
Avinash Lal <lal@college.harvard.edu>
2016-03-10 21:35:50 -0500

    fixed order of pics

--- client/templates/home/sections/board/board.js
+++ client/templates/home/sections/board/board.js
@@ -1,5 +1,14 @@
 Template.board.helpers({
+  president: function() {
+  	return Members.findOne({name: 'Lena Afeyan'});
+  },
+  vicePresident: function() {
+  	return Members.findOne({name: 'Carlo Bocconcelli'});
+  },
   members: function() {
-  	return Members.find({});
+  	return Members.find({$and: [
+  		{name: {$not: 'Carlo Bocconcelli'}},
+  		{name: {$not: 'Lena Afeyan'}}
+  	]});
   }
 });
